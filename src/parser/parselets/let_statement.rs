f143fccbba5de3c28741268d6d5aebabb8ab3494
Alexis Atkinson <alexis.jay.tech@gmail.com>
2026-01-13 22:31:03 -0700

    feat: combine let and try

--- src/parser/parselets/let_statement.rs
+++ src/parser/parselets/let_statement.rs
@@ -1,56 +1,101 @@
-use crate::ast::{LetStmt, Stmt};
+use crate::ast::{BlockStmt, CallExpr, Expr, ExprStmt, IdentifierExpr, LetStmt, Stmt};
 use crate::parser::lexer::{Token, TokenKind};
 use crate::parser::parselets::PrefixStmtParselet;
-use crate::parser::types;
-use crate::parser::{ParseError, Parser};
+use crate::parser::{ParseError, Parser, statement};
 
-/// Parses let statements.
+/// Parses try expressions
 ///
-/// Example: `let x i32`
+/// Example: `let .Ok = do() or { return .Err }`
+/// Example: `let .Ok(buf) = read_file(file) or(e) { print(e); return }`
+/// Example: `let Ret.Ok = send_data(data) !`
 pub struct LetParselet;
 
 impl PrefixStmtParselet for LetParselet {
     fn parse(&self, parser: &mut Parser, _token: Token) -> Result<Stmt, ParseError> {
-        let ident = parser.consume()?;
-        if ident.kind != TokenKind::Identifier {
-            return Err(ParseError::UnexpectedToken(
-                "let statment required an identifier".to_string(),
-            ));
-        }
+        let pattern = statement::parse_pattern(parser)?;
 
-        let type_spec;
-        let mut matched = parser.match_token(TokenKind::Equal)?;
-        if matched {
-            type_spec = None;
-        } else {
-            type_spec = Some(types::parse_type(parser)?);
-            matched = parser.match_token(TokenKind::Equal)?;
-        }
-
-        if !matched && type_spec.is_none() {
-            return Err(ParseError::UnexpectedToken(
-                "let statement must have either a type spec or initialization value".to_string(),
-            ));
-        }
-
-        // this means the statment follows the format of
-        // `let x i32`
+        let matched = parser.match_token(TokenKind::Equal)?;
         if !matched {
-            return Ok(Stmt::Let(LetStmt {
-                name: ident.lexeme,
-                type_annotation: type_spec,
-                value: None,
-            }));
+            return Err(ParseError::UnexpectedToken(format!(
+                "expected ':=' but got {:?}",
+                parser.lookahead(0)?,
+            )));
         }
 
         let value = parser.parse_expression()?;
 
-        // this covers statements like `let x = 5` as well as
-        // `let x bool = true`
-        Ok(Stmt::Let(LetStmt {
-            name: ident.lexeme,
-            type_annotation: type_spec,
-            value: Some(value),
-        }))
+        let next = parser.lookahead(0)?;
+        match next.kind {
+            TokenKind::OrKeyword => {
+                parser.consume()?;
+                let next = parser.lookahead(0)?;
+                let or_binding = if next.kind == TokenKind::OpenParen {
+                    parser.consume()?;
+                    let catch_ident = parser.consume()?;
+                    if catch_ident.kind != TokenKind::Identifier {
+                        return Err(ParseError::UnexpectedToken(format!(
+                            "catch binding must be an identifier, but got {:?}",
+                            catch_ident,
+                        )));
+                    }
+                    parser.match_token(TokenKind::CloseParen)?;
+
+                    Some(Box::new(IdentifierExpr {
+                        name: catch_ident.lexeme,
+                    }))
+                } else {
+                    None
+                };
+
+                let matches = parser.match_token(TokenKind::OpenBrace)?;
+                if !matches {
+                    return Err(ParseError::UnexpectedToken(
+                        "or should be followed by a block".to_string(),
+                    ));
+                }
+
+                let except = Some(statement::parse_block(parser)?);
+
+                Ok(Stmt::Let(LetStmt {
+                    pattern,
+                    value,
+                    or_binding,
+                    except,
+                }))
+            }
+            TokenKind::Bang => {
+                parser.consume()?;
+
+                Ok(Stmt::Let(LetStmt {
+                    pattern,
+                    value,
+                    or_binding: Some(Box::new(IdentifierExpr {
+                        name: "e".to_string(),
+                    })),
+                    except: Some(BlockStmt {
+                        statements: vec![Stmt::Expr(ExprStmt {
+                            expr: Expr::Call(CallExpr {
+                                func: Box::new(Expr::Identifier(IdentifierExpr {
+                                    name: "panic".to_string(),
+                                })),
+                                args: vec![Expr::Identifier(IdentifierExpr {
+                                    name: "e".to_string(),
+                                })],
+                            }),
+                        })],
+                    }),
+                }))
+            }
+            TokenKind::Semicolon => Ok(Stmt::Let(LetStmt {
+                pattern,
+                value,
+                or_binding: None,
+                except: None,
+            })),
+            _ => Err(ParseError::UnexpectedToken(format!(
+                "invalid token after try/catch {:?}",
+                next,
+            ))),
+        }
     }
 }
